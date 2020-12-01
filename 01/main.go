1a2f5192e511f71abdcd93de35dcff1cb703cb47
Mikael Lönn <mikael@sendify.se>
2020-12-01 08:31:32 +0100

    Day 1

--- 01/main.go
+++ 01/main.go
@@ -3,16 +3,46 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
+
 // Part1 Part 1 of puzzle
-func Part1(input string) string {
-	return "Answer: "
+func Part1(input string) string{
+	s := strings.Split(input, "\n")
+	m := make(map[int]int)
+
+	for _, expense := range s {
+		i ,_ := strconv.Atoi(expense)
+		complement := 2020 - i
+		_, found := m[complement]
+		if found {
+			return "Answer: " + strconv.Itoa(i * complement)
+		}
+		m[i] = i
+	}
+	return "Not found"
 }
 
 // Part2 Part2 of puzzle
 func Part2(input string) string {
-	return "Answer: "
+	s := strings.Split(input, "\n")
+	m := make(map[int]int)
+	for _, expense1 := range s {
+		i1 ,_ := strconv.Atoi(expense1)
+		for _, expense2 := range s {
+			i2 ,_ := strconv.Atoi(expense2)
+			complement := 2020 - i1 - i2
+			_, found := m[complement]
+			if found {
+				return "Answer: " + strconv.Itoa(i1*i2*complement)
+			}
+			m[i2] = i2
+		}
+		m[i1] = i1
+	}
+	return "Not found"
 }
 
 func main() {
