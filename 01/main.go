c9342787c6d33719421fd85d6d619e6070cf3b68
Mikael Lönn <mikael@sendify.se>
2020-12-01 11:49:18 +0100

    Add prettier print

--- 01/main.go
+++ 01/main.go
@@ -5,20 +5,20 @@ import (
 	"io/ioutil"
 	"strconv"
 	"strings"
+	"time"
 )
 
 
 // Part1 Part 1 of puzzle
-func Part1(input string) string{
+func Part1(input string) string {
 	s := strings.Split(input, "\n")
 	m := make(map[int]int)
-
 	for _, expense := range s {
 		i ,_ := strconv.Atoi(expense)
 		complement := 2020 - i
 		_, found := m[complement]
 		if found {
-			return "Answer: " + strconv.Itoa(i * complement)
+			return "Answer: " + expense  + " * " + strconv.Itoa(complement) + " = " + strconv.Itoa(i * complement)
 		}
 		m[i] = i
 	}
@@ -36,7 +36,7 @@ func Part2(input string) string {
 			complement := 2020 - i1 - i2
 			_, found := m[complement]
 			if found {
-				return "Answer: " + strconv.Itoa(i1*i2*complement)
+				return "Answer: " + expense1 + " * " + expense2 + " * " + strconv.Itoa(complement) + " = "+ strconv.Itoa(i1*i2*complement)
 			}
 			m[i2] = i2
 		}
@@ -47,7 +47,8 @@ func Part2(input string) string {
 
 func main() {
 	bytes, _ := ioutil.ReadFile("01/input.txt")
-
-	fmt.Println("Part 1: " + Part1(string(bytes)))
-	fmt.Println("Part 2: " + Part2(string(bytes)))
+	start1 := time.Now()
+	fmt.Println("Part 1: " + Part1(string(bytes)), "Time", time.Since(start1))
+	start2 := time.Now()
+	fmt.Println("Part 2: " + Part2(string(bytes)),"Time", time.Since(start2))
 }
