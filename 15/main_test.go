e5a7b0cdaa367a53bf67ef9b65ed3e068ab46639
Mikael Lönn <mikael@sendify.se>
2020-12-16 07:25:11 +0100

    Benchmark 15

--- 15/main_test.go
+++ 15/main_test.go
@@ -7,7 +7,7 @@ import (
 
 func BenchmarkPart1(b *testing.B) {
 	input := utils.ReadInput("../inputs/15.txt")
-	solution := -1
+	solution := 441
 	for n := 0; n < b.N; n++ {
 		answer := Part1(input)
 		if answer != solution {
@@ -18,7 +18,7 @@ func BenchmarkPart1(b *testing.B) {
 
 func BenchmarkPart2(b *testing.B) {
 	input := utils.ReadInput("../inputs/15.txt")
-	solution := -1
+	solution := 10613991
 	for n := 0; n < b.N; n++ {
 		answer := Part2(input)
 		if int(answer) != solution {
