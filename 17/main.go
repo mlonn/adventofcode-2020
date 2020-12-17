e530ffe4ee35cefadf7889e3f491cc66637fd615
Mikael Lönn <mikael@sendify.se>
2020-12-17 13:54:19 +0100

    Use map

--- 17/main.go
+++ 17/main.go
@@ -18,42 +18,12 @@ type Grid struct {
 type Point struct {
 	x, y, z, w int
 }
+
 type world map[Point]bool
 
-var zMin, xMin, yMin int
 var maxX, minX, maxY, minY int
 var cycles = 6
 
-func parseGrid(input string) Grid {
-	lines := strings.Split(input, "\n")
-	h := len(lines)
-	w := len(lines[0])
-	yl := h + cycles*2
-	xl := w + cycles*2
-	zl := 1 + cycles*2
-	grid := make([][][]string, zl)
-	zMin = -cycles
-	yMin = -cycles - h/2
-	xMin = -cycles - w/2
-	for z := zMin; z <= zMin+cycles*2; z++ {
-		grid[z-zMin] = make([][]string, yl)
-		for y := yMin; y < yMin+yl; y++ {
-			grid[z-zMin][y-yMin] = make([]string, xl)
-			for x := range grid[z-zMin][y-yMin] {
-				grid[z-zMin][y-yMin][x] = "."
-			}
-		}
-	}
-
-	for y, line := range lines {
-		for x, position := range line {
-			grid[cycles][y+cycles][x+cycles] = string(position)
-
-		}
-	}
-
-	return Grid{g: grid, xl: xl, yl: yl, zl: zl}
-}
 func parseWorld(input string) world {
 	lines := strings.Split(input, "\n")
 
@@ -62,7 +32,6 @@ func parseWorld(input string) world {
 	minY = math.MaxInt64
 	for y, line := range lines {
 		for x, position := range line {
-
 			if string(position) == "#" {
 				w[Point{x: x, y: y}] = true
 				if x > maxX {
@@ -83,56 +52,6 @@ func parseWorld(input string) world {
 	}
 
 	return w
-}
-
-func (grid Grid) get(x, y, z int) string {
-	if z-zMin < 0 || y-yMin < 0 || x-xMin < 0 {
-		return "."
-	}
-	if x-xMin >= grid.xl || y-yMin >= grid.yl || z-zMin >= grid.zl {
-		return "."
-	}
-	return grid.g[z-zMin][y-yMin][x-xMin]
-}
-
-func (grid Grid) set(x, y, z int, s string) {
-	grid.g[z-zMin][y-yMin][x-xMin] = s
-}
-
-func (grid Grid) isActive(x, y, z int) bool {
-	return grid.get(x, y, z) == "#"
-}
-
-func (grid Grid) isInactive(x, y, z int) bool {
-	return grid.get(x, y, z) == "."
-}
-
-func (grid Grid) next(x, y, z int) string {
-	active := 0
-	for dz := z - 1; dz <= z+1; dz++ {
-		for dy := y - 1; dy <= y+1; dy++ {
-			for dx := x - 1; dx <= x+1; dx++ {
-				if (dx != x) || (dy != y) || (dz != z) {
-					if grid.isActive(dx, dy, dz) {
-						active++
-					}
-				}
-			}
-		}
-	}
-
-	if grid.isInactive(x, y, z) {
-		if active == 3 {
-			return "#"
-		}
-	}
-
-	if grid.isActive(x, y, z) {
-		if active != 2 && active != 3 {
-			return "."
-		}
-	}
-	return grid.get(x, y, z)
 }
 
 func (w world) next(p Point) bool {
@@ -165,20 +84,6 @@ func (w world) next(p Point) bool {
 	return w[p]
 }
 
-func (grid Grid) getActive() int {
-	active := 0
-	for z, slice := range grid.g {
-		for y, row := range slice {
-			for x := range row {
-				if grid.g[z][y][x] == "#" {
-					active++
-				}
-			}
-		}
-	}
-	return active
-}
-
 func (w world) getActive() int {
 	active := 0
 	for _, a := range w {
@@ -191,20 +96,28 @@ func (w world) getActive() int {
 
 // Part1 Part 1 of puzzle
 func Part1(input string) int {
-	current := parseGrid(input)
-	next := parseGrid(input)
+	current := parseWorld(input)
+	next := parseWorld(input)
 	cycle := 0
-	//minW := 0
-	//maxW := 0
-	fmt.Println(maxX, minX, maxY, minY)
+	minZ := 0
+	maxZ := 0
 	for cycle < cycles {
-		for z := zMin; z < zMin+len(current.g); z++ {
-			for y := yMin; y < yMin+len(current.g[z-zMin]); y++ {
-				for x := xMin; x < xMin+len(current.g[z-zMin][y-yMin]); x++ {
-					next.set(x, y, z, current.next(x, y, z))
+		minX--
+		minY--
+		minZ--
+		maxX++
+		maxY++
+		maxZ++
+
+		for z := minZ; z <= maxZ; z++ {
+			for y := minY; y <= maxY; y++ {
+				for x := minX; x <= maxX; x++ {
+					p := Point{x: x, y: y, z: z}
+					next[p] = current.next(p)
 				}
 			}
 		}
+
 		current, next = next, current
 		cycle++
 	}
